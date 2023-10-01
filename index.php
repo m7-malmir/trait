2acc8f1ae835a0072725aed544f64625982680eb
Mohammad Malmir <m.malmir666@gmail.com>
2023-10-01 23:18:02 +0330

    trait file is added

--- index.php
+++ index.php
@@ -1,18 +1,33 @@
 <?php
-
-trait MyTrait{
-    public function fly(){
-        echo 'I am flying';
+trait FlyableTrait{
+   abstract public function fly();
+   public function hello(){
+    echo 'Hello i can fly';
+}
+}
+trait EngineTrait{
+    public function hello(){
+        echo 'I have an engine';
     }
 }
 class Plain{
-    use MyTrait;
+    use FlyableTrait,EngineTrait{
+        FlyableTrait::hello insteadof EngineTrait;
+        EngineTrait::hello as engineHello;
+    }
+    public function fly(){
+        echo 'i am flying 800 km/h';
+    }
 }
-class Helycopter{
-    use MyTrait;
-}
-
-$plain=new Plain;
-$helycopter=new Helycopter;
-$plain->fly();
-$helycopter->fly();
+// class Helycopter{
+//     use FlyableTrait,EngineTrait;
+//     public function fly(){
+//         echo 'i am flying 300 km/h';
+//     }
+// }
+// class Car{
+//     use EngineTrait;
+// }
+$plain=new Plain();
+//$plain->fly();
+$plain->engineHello();
---
+++ trait.php
@@ -0,0 +1,19 @@
+<?php
+
+class A{
+    public $var1=10;
+    public function hello(){
+        echo 'Hello from  class A';
+    }
+}
+trait traitA
+{
+    public function hello(){
+        echo 'Hello from  trait A'. parent::hello().$this->var1;
+    }
+}
+class B extends A{
+    use traitA;
+}
+$ob=new B();
+$ob->hello();
